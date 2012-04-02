7035bca8b5e62ebe64f918d908ae6e5acddb0f81
Caroline Braud <caroline.braud@univ-lille1.fr>
2012-04-02 19:08:28 +0200

    Class incorporated, serial.get.cpp created to get value from serial port

--- Makefile
+++ Makefile
@@ -8,9 +8,9 @@ CPP = g++
 
 all: $(PROGRAMS) $(DOCUMENTATIONS)
 
-rs232: serialCOM.cpp  
-	$(CPP) $(OPT) serialCOM.cpp -o $@
-doc: rs232.Doxygen serialCOM.cpp 
+rs232: serialCOM_v2.cpp  
+	$(CPP) $(OPT) serialCOM_v2.cpp -o $@
+doc: rs232.Doxygen serialCOM_v2.cpp 
 	./doxIt.sh
 
 clean:
--- serialCOM.cpp
+++ serialCOM.cpp
@@ -16,8 +16,103 @@
      */
 using namespace std;
 
-int main(void) 
+class serialCOM(void) 
 { 
+  string port;
+  string message;
+ public:
+  
+  //! Open serial port
+  /** 
+   *
+   * @param[in] port 
+   *
+   * @return 
+   */
+  int serial_open(string port)
+  {
+    int fd; /* File descriptor for the port */  
+    fd = open((const char*)port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
+    if (fd == -1)
+      {
+	perror("open_port: Unable to open /dev/ttyUSB0 - ");
+      }
+    else	
+      {
+	fcntl(fd, F_SETFL, 0);
+	cerr << "port is open"<<endl;
+      }  
+    return(fd);
+  }
+  
+  //! write on serial port
+  /** 
+   *
+   * @param[in] fd= state of port 
+   * @param[in] message= string to send to serial port  
+   *
+   * @return 
+   */
+  int serial_write(int fd,string message)
+  {
+    int  tries=0;        /* Number of tries so far */
+    message.append("\r");
+    //cout << message << endl;
+    //cout << message.size()<< endl;
+    while (write(fd,(const char*)message.c_str(),message.size()) <message.size())
+      {
+	tries++;
+	//    {
+	//    if (write(fd,(const char*)message.c_str(),message.size()) <message.size())
+	// 	  break;
+	if (tries > 3)
+	  {
+	    cerr << "write KO :(" << endl;
+	    return 1;
+	  }
+	cerr << "rs232 not ready, retry" << endl;
+      }
+    cerr << "write OK" << endl;
+    return 0;
+  }
+
+  //! write on serial port
+  /** 
+   *
+   * @param[in] fd= state of port 
+   * @param[out] value= value returned by serial port
+   *
+   * @return 
+   */
+  int serial_read(int fd,string &value)
+  {
+    char buffer[255];  /* Input buffer */
+    char *bufptr;      /* Current char in buffer */
+    int  nbytes;       /* Number of bytes read */
+    
+    bufptr = buffer;
+    while ((nbytes = read(fd, bufptr, buffer + sizeof(buffer) - bufptr - 1)) > 0)
+      {
+	//cout<<"nbytes="<<nbytes<<"\n";
+	bufptr += nbytes;
+	if (bufptr[-1] == '\n' || bufptr[-1] == '\r')
+	  break;
+      }
+    buffer[bufptr-buffer-1]='\0';
+    //cout << buffer << endl;
+    close(fd);
+    value=buffer;
+    return (0);
+  } 
+
+};
+
+
+
+
+
+
+
   // OPEN 
   string port="/dev/ttyUSB0";
   int fd=serial_open(port);
@@ -29,99 +124,6 @@ int main(void)
   serial_read(fd,value);
   cout << value << endl;
    return (0);
-}
-
-//! Open serial port
-/** 
- *
- * @param[in] port 
- *
- * @return 
- */
-int serial_open(string port)
-{
-  int fd; /* File descriptor for the port */  
-  fd = open((const char*)port.c_str(), O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1)
-    {
-      perror("open_port: Unable to open /dev/ttyUSB0 - ");
-    }
-  else	
-    {
-      fcntl(fd, F_SETFL, 0);
-      cerr << "port is open"<<endl;
-    }  
-  return(fd);
-}
-
-//! write on serial port
-/** 
- *
- * @param[in] fd= state of port 
- * @param[in] message= string to send to serial port  
- *
- * @return 
- */
-int serial_write(int fd,string message)
-{
-  int  tries=0;        /* Number of tries so far */
-  message.append("\r");
-  //cout << message << endl;
-  //cout << message.size()<< endl;
-  while (write(fd,(const char*)message.c_str(),message.size()) <message.size())
-    {
-      tries++;
-  //    {
-  //    if (write(fd,(const char*)message.c_str(),message.size()) <message.size())
-  // 	  break;
-      if (tries > 3)
-	{
-	  cerr << "write KO :(" << endl;
-	  return 1;
-	}
-      cerr << "rs232 not ready, retry" << endl;
-    }
-    cerr << "write OK" << endl;
-    return 0;
-}
-
-//! write on serial port
-/** 
- *
- * @param[in] fd= state of port 
- * @param[out] value= value returned by serial port
- *
- * @return 
- */
-int serial_read(int fd,string &value)
-{
-  char buffer[255];  /* Input buffer */
-  char *bufptr;      /* Current char in buffer */
-  int  nbytes;       /* Number of bytes read */
-
-	bufptr = buffer;
-	while ((nbytes = read(fd, bufptr, buffer + sizeof(buffer) - bufptr - 1)) > 0)
-	  {
-	    //cout<<"nbytes="<<nbytes<<"\n";
-	    bufptr += nbytes;
-	    if (bufptr[-1] == '\n' || bufptr[-1] == '\r')
-	      break;
-	  }
-	buffer[bufptr-buffer-1]='\0';
-	//cout << buffer << endl;
-	close(fd);
-	value=buffer;
-   return (0);
-}
-
-
-
-
-
-
-
-
-
 
 
 
