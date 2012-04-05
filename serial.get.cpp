521de224a94cee0d90bb2580dc9544a040945504
Caroline Braud <caroline.braud@univ-lille1.fr>
2012-04-05 10:50:54 +0200

    Add Cimg ok

--- serial.get.cpp
+++ serial.get.cpp
@@ -5,25 +5,40 @@
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
+#define cimg_debug         2
+
+//CIMG Library
+#include "../CImg/CImg.h"
+//#include "../CImg.Tool/CImg_NetCDF.h"
 
 using namespace std;
 
+//class serial communication
 #include "serialCOM.h"
 
 //!todo: add CIMG library for command lines
 
-int main(void)
+int main(int argc, char *argv[])
 { 
+  const int         verbose    = (cimg_option("-v",(const char*)NULL,"verbose option")!=NULL);
+  const bool        show_info  = (cimg_option("--info",(const char*)NULL,"information")!=NULL);
+  const bool        show_help  = (cimg_option("-h",(const char*)NULL,"information")!=NULL);
+  const std::string SerialPath =  cimg_option("--path","/dev/ttyUSB0","Path serial device");
+  const std::string Message =  cimg_option("--message","*IDN?","Message to send to the serial port (the example ask if IFA300 ready)");
+
+  if (show_help|show_info)
+    return 0; // program stops after printing the information
+
   serialCOM::serialCOM serial;
 
   //string port;
   //string message;
 
   // OPEN 
-  serial.port="/dev/ttyUSB0";
+  serial.port=SerialPath;
   int fd=serial.opens();
   // WRITE 
-  serial.message="INST:CNUM?";
+  serial.message=Message;
   serial.writes(fd);
   // READ
   string value;
--- serial.put.cpp
+++ serial.put.cpp
@@ -5,6 +5,10 @@
 #include <fcntl.h>   /* File control definitions */
 #include <errno.h>   /* Error number definitions */
 #include <termios.h> /* POSIX terminal control definitions */
+#define cimg_debug         2
+
+//CIMG Library
+#include "../CImg/CImg.h"
 
 using namespace std;
 
@@ -12,18 +16,27 @@ using namespace std;
 
 //!todo: add CIMG library for command lines
 
-int main(void)
+int main(int argc, char *argv[])
 { 
+  const int         verbose    = (cimg_option("-v",(const char*)NULL,"verbose option")!=NULL);
+  const bool        show_info  = (cimg_option("--info",(const char*)NULL,"information")!=NULL);
+  const bool        show_help  = (cimg_option("-h",(const char*)NULL,"information")!=NULL);
+  const std::string SerialPath =  cimg_option("--path","/dev/ttyUSB0","Path serial device");
+  const std::string Message =  cimg_option("--message","CONF:GAIN 1","Message to write to serial port (the example set the IFA300 gain to 1");
+
+  if (show_help|show_info)
+    return 0; // program stops after printing the information
+
   serialCOM::serialCOM serial;
 
   //string port;
   //string message;
 
   // OPEN 
-  serial.port="/dev/ttyUSB0";
+  serial.port=SerialPath;
   int fd=serial.opens();
   // WRITE 
- serial.message="INST:CNUM?";
+ serial.message=Message;
  cerr << "write:" << serial.message <<endl;
   serial.writes(fd);
 }
